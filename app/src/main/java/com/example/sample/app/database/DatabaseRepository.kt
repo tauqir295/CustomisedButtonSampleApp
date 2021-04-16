88f5264cde89181d8e4d605d6ac077a715e9bf3c
Mohammad Tauqir <tauqir295@gmail.com>
2021-04-16 15:23:07 +0530

    code cleanup

--- app/src/main/java/com/example/sample/app/database/DatabaseRepository.kt
+++ app/src/main/java/com/example/sample/app/database/DatabaseRepository.kt
@@ -1,5 +1,6 @@
 package com.example.sample.app.database
 
+import androidx.annotation.WorkerThread
 import com.example.sample.app.database.model.User
 import javax.inject.Inject
 
@@ -8,10 +9,12 @@ class DatabaseRepository @Inject constructor(private val userDao: UserDao) {
     // By default Room runs suspend queries off the main thread, therefore, we don't need to
     // implement anything else to ensure we're not doing long running database work
     // off the main thread.
+    @WorkerThread
     suspend fun insert(user: User) {
         userDao.insert(user)
     }
 
+    @WorkerThread
     suspend fun getUser(userName: String, password: String): String? {
         return userDao.findByUserNameAndPassword(userName, password)
     }
